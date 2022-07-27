72d9377ce5d4e7851c7b55e3ca28bb9cd86c2024
Muhammad Haris <haris@debian-BULLSEYE-live-builder-AMD64>
2022-07-27 15:19:49 +0500

    contact completed respon

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-// select all li elements
-var li = document.querySelectorAll(".nav-item");
-// loop through the array
-for (var i = 0; i < li.length; i++) {
-  // add event listener on click
-  li[i].addEventListener("click", function () {
-    // remove class 'selected' from all li elements
-    // console log the element text content that was clicked
-    console.log(this.textContent);
-    for (var j = 0; j < li.length; j++) {
-      li[j].classList.remove("active");
-      if (li[j].textContent == this.textContent) {
-        li[j].classList.add("active");
-      }
-    }
-    // add class 'selected' to the clicked li element
-    this.classList.add("active");
-  });
-}
+// // select all li elements
+// var li = document.querySelectorAll(".nav-item");
+// // loop through the array
+// for (var i = 0; i < li.length; i++) {
+//   // add event listener on click
+//   li[i].addEventListener("click", function () {
+//     // remove class 'selected' from all li elements
+//     // console log the element text content that was clicked
+//     console.log(this.textContent);
+//     for (var j = 0; j < li.length; j++) {
+//       li[j].classList.remove("active");
+//       if (li[j].textContent == this.textContent) {
+//         li[j].classList.add("active");
+//       }
+//     }
+//     // add class 'selected' to the clicked li element
+//     this.classList.add("active");
+//   });
+// }
 
 // Responsive sidebar navigation
 function openNav() {
